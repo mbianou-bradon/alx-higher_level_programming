3c940eddd290051cbdd9252bc31765bc6b3028fd
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-06-26 13:41:02 -0700

    Completed task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
+
 
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
+        return True
     except Exception as e:
-        print("Exception:", e, file=sys.stderr)
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
-    return True
