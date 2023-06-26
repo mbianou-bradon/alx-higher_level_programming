76ae2eb1dd2d317a105db4afa1efd8410c782ce6
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-06-26 13:30:58 -0700

    Completed task 0 to 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    try:
-        while i < x:
-            print("{:d}".format(my_list[i]), end="")
-            i += 1
-    except:
-        pass
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except Exception as e:
+            break
+        else:
+            count += 1
     print()
-    return i
+    return count
