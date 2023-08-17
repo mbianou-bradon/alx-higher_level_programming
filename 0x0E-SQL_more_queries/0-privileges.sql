7e40db63719e1964a9a17a1851025b44e38eda07
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-08-16 23:34:58 -0700

    Updated task 0 and task1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- My privileges
--- show privs for user1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- script to list privelages of users on server
+-- DML QUERY to do above
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
--- Creates user
--- create user
+-- script to create a new user in server
+-- DDL query to do so.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- grant privs
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- flush
-FLUSH PRIVILEGES;
+-- query to grant perms to user
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
