80a29a288ba547f1f94012ce99f566810da548f9
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-11 18:18:43 -0700

    Completed task 1 and 2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const lang1 = 'C is fun';
+const lang2 = 'Python is cool';
+const lang3 = 'Javascript is amazing';
+
+console.log(lang1);
+console.log(lang2);
+console.log(lang3);
