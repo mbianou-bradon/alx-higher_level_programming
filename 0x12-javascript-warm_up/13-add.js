c1282840df62ceed44737a6b03c241da90082a61
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-11 19:35:21 -0700

    Completed task 11 to 13

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/*
+returns the addition of 2 integers.
+http://51elliot.blogspot.com/2012/01/simple-intro-to-nodejs-module-scope.html
+*/
+
+exports.add = function (a, b) {
+  return (a + b);
+};
