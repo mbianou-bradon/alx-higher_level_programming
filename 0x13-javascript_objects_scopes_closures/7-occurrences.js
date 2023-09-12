a4f587e930ef10fd035278f8fa386fe3c4ab201a
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-09-12 16:31:12 -0700

    Completed task 7 and 8

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * Describes function that returns the number of occurrences in a list
+*/
+
+exports.nbOccurences = function (list, searchElement) {
+  return (list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0));
+};
